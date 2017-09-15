9eef4dba4a824539768983f930fab1ba6b621ffa
thughey <timhughey@mac.com>
2017-09-14 21:30:56 -0400

    added IExHelpers and new mcp template

---
+++ apps/mcp.new/lib/mcp.ex
@@ -0,0 +1,18 @@
+defmodule Mcp do
+  @moduledoc """
+  Documentation for Mcp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mcp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
