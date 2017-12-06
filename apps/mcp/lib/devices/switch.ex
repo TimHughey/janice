46ca46f9f1aa0f8c366b8afef1309fd3c5e59f76
Tim Hughey <thughey@icloud.com>
2017-12-05 19:45:18 -0500

    - added humanize for displaying seconds - added scripts to build and deploy

--- apps/mcp/lib/devices/switch.ex
+++ apps/mcp/lib/devices/switch.ex
@@ -90,7 +90,7 @@ end
 
 def get(:friendly_name, fname)
 when is_binary(fname) do
-  %{device: dev, _} = fname_to_dev(fname) |> device_name_and_pio()
+  %{device: dev} = fname_to_dev(fname) |> device_name_and_pio()
   get_by_device_name(dev)
 end
 
--- apps/web/lib/web/controllers/mcp_controller.ex
+++ apps/web/lib/web/controllers/mcp_controller.ex
@@ -74,7 +74,7 @@ defmodule Web.McpController do
     %{device: s.device, fname: fname, desc: a.description,
       type: s.sensor_type,
       reading_at: s.reading_at, last_seen_at: s.last_seen_at,
-      last_seen_secs: last_seen_secs}
+      last_seen_secs: humanize_secs(last_seen_secs)}
   end
 
   defp switch_details(fname) do
@@ -90,10 +90,10 @@ defmodule Web.McpController do
 
     %{device: a.device, fname: a.friendly_name, desc: a.description,
       enabled: s.enabled, dev_latency: s.dev_latency,
-      discovered_at: Timex.format(s.discovered_at, "{ISO:Extended:Z}"),
-      last_cmd_secs: last_cmd_secs,
-      last_seen_secs: last_seen_secs,
-      last_seen_at: Timex.format(s.last_seen_at, "{ISO:Extended:Z}"),
+      discovered_at: Timex.format!(s.discovered_at, "{ISO:Extended:Z}"),
+      last_cmd_secs: humanize_secs(last_cmd_secs),
+      last_seen_secs: humanize_secs(last_seen_secs),
+      last_seen_at: Timex.format!(s.last_seen_at, "{ISO:Extended:Z}"),
       state: Switch.get_state(a.friendly_name)}
   end
 
@@ -106,4 +106,33 @@ defmodule Web.McpController do
       last_seen_at: "?",
       state: "?"}
   end
+
+  # one (1) week: 604_800  
+  # one (1) day : 86,400
+  # one (1) hour: 3_600
+
+  defp humanize_secs(secs)
+  when secs >= 604_800 do
+    ">1 week"
+  end
+
+  defp humanize_secs(secs)
+  when secs >= 86_400 do
+    ">1 day"
+  end
+
+  defp humanize_secs(secs)
+  when secs >= 3_600 do
+    ">1 hr"
+  end
+ 
+  defp humanize_secs(secs)
+  when secs >= 60 do
+    ">1 min"
+  end
+
+  defp humanize_secs(secs) do
+    "#{secs} secs"
+  end 
+  
 end
