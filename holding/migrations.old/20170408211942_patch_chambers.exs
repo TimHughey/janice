89631eebd092186786ab28dfef16259111cad62b
thughey <timhughey@mac.com>
2017-12-20 22:28:18 -0600

    -misc removal of deprecated files -cleanup of .gitignore, platform.ini

--- holding/migrations.old/20170408211942_patch_chambers.exs
+++
@@ -1,9 +0,0 @@
-defmodule Mcp.Repo.Migrations.PatchChambers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:chambers) do
-      add :relh_control_ms, :integer, null: false, default: 30_000
-    end
-  end
-end
---
+++ mcp/README.md
@@ -0,0 +1,3 @@
+# Master Control Remote
+
+Placeholder for README
