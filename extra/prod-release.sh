b60214f80b5eec93968c68f3a98d5f1ded4364b2
Tim Hughey <thughey@icloud.com>
2018-03-30 20:07:13 -0400

    -corrected prod build for Angular UI

--- extra/prod-release.sh
+++ extra/prod-release.sh
@@ -46,8 +46,8 @@ run_cmd env MIX_ENV=prod mix phx.digest
 
 cd $MCR
 print -n "building mcr_esp..."
-run_cmd make app-clean 1> /dev/null 2>&1
-run_cmd make -j9 deploy 1> /dev/null 2>&1 && print " done"
+run_cmd make app-clean 
+run_cmd make -j9 deploy-to-mcp && print " done"
 
 cd $MCP
 run_cmd env MIX_ENV=prod mix release --env=prod
