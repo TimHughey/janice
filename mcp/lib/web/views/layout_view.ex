621cd0c4c1ce0a5cd6a38ffd6776b8f3496f8b97
Tim Hughey <thughey@icloud.com>
2018-05-11 17:20:34 -0400

    -yet another attempt to fix this darn brunch vs webpack issue

--- mcp/lib/web/views/layout_view.ex
+++ mcp/lib/web/views/layout_view.ex
@@ -8,8 +8,8 @@ defmodule Web.LayoutView do
 
     {:ok, files} = File.ls(bundles_path)
 
-    styles_re = ~r/(?<file>^[a-z]+\.[[:xdigit]]+\.bundle\.css$)/
-    js_re = ~r/(?<file>^[a-z]+\.[[:xdigit:]]+\.bundle\.js$)/
+    styles_re = ~r/(?<file>styles\.[[:xdigit]]+\.bundle\.css)/
+    js_re = ~r/(?<file>[a-z]+\.[[:xdigit:]]+\.bundle\.js)/
 
     ss =
       for f <- files, match = Regex.named_captures(styles_re, f) do
@@ -33,8 +33,6 @@ defmodule Web.LayoutView do
     #   for f <- files, do: "#{f}.bundle.js"
     # else
     %{ss: _, js: js_files} = locate_ui_files()
-
-    r = ~r/[a-z]+\.[[:xdigit:]]+\.bundle\.js/
 
     for f <- files do
       Enum.find(js_files, fn x -> String.contains?(x, f) end)
