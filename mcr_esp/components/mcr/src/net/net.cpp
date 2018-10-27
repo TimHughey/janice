8e85f50fb6e91833376bca3fc8dab570e94c4004
Tim Hughey <thughey@icloud.com>
2018-10-27 14:45:16 -0400

    -switched to using DNS aliases for NTP

--- mcr_esp/components/mcr/src/net/net.cpp
+++ mcr_esp/components/mcr/src/net/net.cpp
@@ -233,8 +233,8 @@ bool Net::start() {
 
   if (waitForIP()) {
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    sntp_setservername(0, (char *)"jophiel.wisslanding.com");
-    sntp_setservername(1, (char *)"loki.wisslanding.com");
+    sntp_setservername(0, (char *)"ntp1.wisslanding.com");
+    sntp_setservername(1, (char *)"gabriel.wisslanding.com");
     sntp_init();
 
     ensureTimeIsSet();
