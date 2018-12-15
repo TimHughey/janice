95080885dca59d3c6e6c98dba406b0c5672b9744
Tim Hughey <thughey@icloud.com>
2018-12-15 15:19:47 -0500

    -added :help options for Jobs

--- mcp/lib/scheduler/jobs.ex
+++ mcp/lib/scheduler/jobs.ex
@@ -1,6 +1,7 @@
 defmodule Janice.Jobs do
   @moduledoc false
   require Logger
+  import IO.ANSI
 
   def flush do
     thermo = "grow heat"
@@ -36,6 +37,28 @@ defmodule Janice.Jobs do
       Logger.info(fn -> "thermostat #{inspect(thermo)} set to #{inspect(profile)}" end)
       Thermostat.Server.activate_profile(thermo, profile)
     end
+  end
+
+  def help do
+    IO.puts("reefwater(:atom) -> control reefwater mix system")
+    IO.puts("sump(:atom)      -> control display tank replenish")
+    IO.puts(" ")
+    IO.puts(yellow <> ":help displays the various options for each" <> reset)
+  end
+
+  def reefwater(:help) do
+    IO.puts(":standby        -> all subsystems on standby")
+    IO.puts("                -> heat=low energy")
+    IO.puts(":change         -> air=off, pump=on, replenish=off, fill=off")
+    IO.puts("                -> heat=match display tank")
+    IO.puts(":mix            -> air=off, pump=high, replenish=fast, fill=off")
+    IO.puts("                -> heat=match display tank")
+    IO.puts(":fill_daytime   -> air=off, pump=low, replenish=slow, fill=slow")
+    IO.puts("                -> heat=match display tank")
+    IO.puts(":fill_overnight -> air=off, pump=low, replenish=slow, fill=fast")
+    IO.puts("                -> heat=match display tank")
+    IO.puts(":eco            -> air=off, pump=low, replenish=fast, fill=standby")
+    IO.puts("                -> heat=low energy")
   end
 
   def reefwater(:change) do
@@ -97,7 +120,7 @@ defmodule Janice.Jobs do
       {"reefwater mix air", "off"},
       {"reefwater mix pump", "low"},
       {"display tank replenish", "fast"},
-      {"reefwater rodi fill", "off"}
+      {"reefwater rodi fill", "standby"}
     ]
 
     for {dc, p} <- dcs, do: Dutycycle.Server.activate_profile(dc, p, enable: true)
@@ -116,6 +139,11 @@ defmodule Janice.Jobs do
     for {dc, p} <- dcs, do: Dutycycle.Server.activate_profile(dc, p, enable: true)
 
     Thermostat.Server.activate_profile("reefwater mix heat", "standby")
+  end
+
+  def sump(:help) do
+    IO.puts(":standby -> replenish=standby")
+    IO.puts(":resume  -> replenish=fast")
   end
 
   def sump(:standby) do
