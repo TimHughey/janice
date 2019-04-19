3c44e14a3d7429a07f43c30c17f82647c2d685b3
Tim Hughey <timhughey@mac.com>
2019-04-18 20:17:21 -0400

    Remove Legacy MCR Flash Script

--- extra/mcr-esp-flash-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-
-cd ${HOME}/devel/janice && git pull --tags
-
-
-ssh loki "cd ${HOME}/devel/janice && git pull && ./extra/mcr-esp-flash-local.sh"
-ssh odin "cd ${HOME}/devel/janice && git pull && ./extra/mcr-esp-flash-local.sh"
-./extra/mcr-esp-flash-local
