4a9b42213355342cee58395c2d14bd642c555a5a
Tim Hughey <timhughey@mac.com>
2019-04-24 22:20:09 -0400

    Fixed Install and Start Script

--- extra/install-and-start.sh
+++ extra/install-and-start.sh
@@ -51,11 +51,6 @@
 	run_cmd sudo chown janice:janice $jan_base_new
 	run_cmd sudo -u janice tar -C $jan_base_new -xf $release && print " done."
 
-	print -n "copying mcr_esp.{bin,elf} to htdocs..."
-	run_cmd sudo -u janice cp ${mcr_esp_bin_src} ${mcr_esp_bin_deploy}
-	run_cmd sudo -u janice cp ${mcr_esp_elf_src} ${mcr_esp_elf_deploy}
-	print " done."
-
 	$jan_bin/mcp ping 1> /dev/null 2>&1
 	if [[ $? -eq 0 ]]; then
 		print -n "stopping janice... "
@@ -65,7 +60,7 @@
 
 		# check mcp really shutdown
 		$jan_bin/mcp ping 1> /dev/null 2>&1
-		if [[$? -eq 0 ]]; then
+		if [[ $? -eq 0 ]]; then
 		  print "FAILED, aborting install."
 		  return 1
 		else
@@ -89,19 +84,6 @@
 	sudo -u janice --set-home --preserve-env env PORT=4009 $jan_bin/mcp start && print " done."
 	# sudo -u janice env PORT=4009 $jan_bin/mcp start && print " done."
 	# sleep 5 ; sudo chmod go+w /run
-
-	print -n "creating links to latest mcr_esp.{bin,elf}..."
-
-	cd $mcr_esp_fw_loc
-	run_cmd sudo -u janice rm -rf \
-				${mcr_esp_fw_loc}/latest-mcr_esp.bin \
-				${mcr_esp_fw_loc}/latest-mcr_esp.elf
-
-	run_cmd sudo -u janice ln -s $mcr_esp_bin ./latest-mcr_esp.bin
-	run_cmd sudo -u janice ln -s $mcr_esp_elf ./latest-mcr_esp.elf
-	cd $save_cwd
-
-	print "done."
 
 	print -n "removing deploy tar..." && rm -f $release && print " done."
 
