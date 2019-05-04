268cd7df830f6bd47f58455d79a3d339504ee8e4
Tim Hughey <timhughey@mac.com>
2019-05-04 09:38:14 -0400

    MCR Set ONLY i2cEngine tag to INFO Loggin

--- mcr_esp/components/mcr/src/engines/i2c_engine.cpp
+++ mcr_esp/components/mcr/src/engines/i2c_engine.cpp
@@ -52,8 +52,8 @@ static mcrI2c_t *__singleton__ = nullptr;
 
 mcrI2c::mcrI2c() {
   setTags(localTags());
-  setLoggingLevel(ESP_LOG_INFO);
-  // setLoggingLevel(tagEngine(), ESP_LOG_INFO);
+  setLoggingLevel(ESP_LOG_WARN);
+  setLoggingLevel(tagEngine(), ESP_LOG_INFO);
   // setLoggingLevel(tagDetectDev(), ESP_LOG_INFO);
   // setLoggingLevel(tagDiscover(), ESP_LOG_INFO);
   // setLoggingLevel(tagReport(), ESP_LOG_INFO);
