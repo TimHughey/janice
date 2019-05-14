e2be33630151bd38061c4ecf5b9dd249fd8ef0a8
Tim Hughey <timhughey@mac.com>
2019-05-14 14:37:46 -0400

    Rename of MCR Firmware Tracker

---
+++ extra/mcr/track-fw-size.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+janice=${HOME}/devel/janice
+extra=${janice}/extra
+mcr_base=${janice}/mcr_esp
+tracker=${extra}/mcr/firmware-size-tracker.txt
+
+pushd $mcr_base
+
+# the first make ensures everything is compiled
+make size 1>/dev/null || exit 1
+
+# the second make records the clean output
+echo ">>>" >> ${tracker}
+git log -1 1>>${tracker}
+make size 1>>${tracker}
+echo "<<<" >> ${tracker}
+
+popd
