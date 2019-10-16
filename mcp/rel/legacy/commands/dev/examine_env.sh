de8e0d8cd705d029ebc90ce02ca018f761faf36d
Tim Hughey <timhughey@mac.com>
2019-10-16 12:53:57 -0400

    Removed MCP legacy Distillery configuration

--- mcp/rel/legacy/commands/dev/examine_env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/#!/usr/bin/env zsh
-
-echo ${SHELL}
-echo "name: ${REL_NAME}  vsn: ${REL_VSN}"
--- mcp/rel/legacy/hooks/dev/pre_configure.d/set_host.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env zsh
-
-export REPLACE_OS_VARS=true
-export MCP_HOST
-
-MCP_HOST=$(hostname -f)
