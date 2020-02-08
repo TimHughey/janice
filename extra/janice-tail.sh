b3bd63aef96bc284424ea36676ab03948f8b17e2
Tim Hughey <timhughey@mac.com>
2020-02-08 16:12:34 -0500

    Final Janice Tail Script and Prod SQL Structure

--- extra/janice-tail.sh
+++ extra/janice-tail.sh
@@ -4,19 +4,15 @@ jan_base=/usr/local/janice
 jan_bin=${jan_base}/bin
 mcp=${jan_bin}/mcp
 
-sleep 2
-mcp_pid=$(sudo -u janice --login $jan_bin/mcp pid)
-
-
-exec tail --lines=100 --pid=${mcp_pid} -f $jan_base/tmp/log/erlang.*(om[1])
-
 print -n "waiting for mcp to start... "
 
-until mcp_pid=$($mcp pid >2 /dev/null); do
+until $mcp pid >2 /dev/null; do
   sleep 1
 done
 
 print "done."
+
+mcp_pid=$($mcp pid)
 
 print "tailing janice log file. (use CTRL+C to stop)"
 
