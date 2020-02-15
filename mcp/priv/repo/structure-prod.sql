044dd65218eee303c97650158b72c0c7758bd54b
Tim Hughey <timhughey@mac.com>
2020-02-15 11:48:10 -0500

    Updated Prod SQL Structure

--- mcp/priv/repo/structure-prod.sql
+++ mcp/priv/repo/structure-prod.sql
@@ -373,13 +373,13 @@ ALTER SEQUENCE public.sensor_temperature_id_seq OWNED BY public.sensor_temperatu
 CREATE TABLE public.switch (
     id bigint NOT NULL,
     device character varying(40) NOT NULL,
-    enabled boolean DEFAULT true NOT NULL,
     dev_latency bigint DEFAULT 0 NOT NULL,
     discovered_at timestamp without time zone DEFAULT timezone('utc'::text, now()),
     last_cmd_at timestamp without time zone DEFAULT (timezone('utc'::text, now()) - '03:00:00'::interval),
     last_seen_at timestamp without time zone DEFAULT timezone('utc'::text, now()),
     inserted_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    updated_at timestamp without time zone NOT NULL,
+    log boolean DEFAULT false
 );
 
 
@@ -486,7 +486,9 @@ CREATE TABLE public.switch_state (
     state boolean DEFAULT false NOT NULL,
     ttl_ms integer DEFAULT 60000 NOT NULL,
     inserted_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    updated_at timestamp without time zone NOT NULL,
+    log boolean DEFAULT false,
+    invert_state boolean DEFAULT false
 );
 
 
@@ -1082,5 +1084,5 @@ ALTER TABLE ONLY public.thermostat_profile
 -- PostgreSQL database dump complete
 --
 
-INSERT INTO public."schema_migrations" (version) VALUES (20171217150128), (20171224164529), (20171224225113), (20171228191703), (20171229001359), (20171231182344), (20180101153253), (20180102171624), (20180102175335), (20180217212153), (20180218021213), (20180222165118), (20180222184042), (20180305193804), (20180307143400), (20180517201719), (20180708221600), (20180709181021), (20190308124055), (20190316032007), (20190317155502), (20190320124824), (20190416130912), (20190417011910), (20191018110319), (20191022013914), (20200105131440), (20200115151705), (20200116024319), (20200127033742), (20200128032134), (20200210202655), (20200212175538), (20200212183409);
+INSERT INTO public."schema_migrations" (version) VALUES (20171217150128), (20171224164529), (20171224225113), (20171228191703), (20171229001359), (20171231182344), (20180101153253), (20180102171624), (20180102175335), (20180217212153), (20180218021213), (20180222165118), (20180222184042), (20180305193804), (20180307143400), (20180517201719), (20180708221600), (20180709181021), (20190308124055), (20190316032007), (20190317155502), (20190320124824), (20190416130912), (20190417011910), (20191018110319), (20191022013914), (20200105131440), (20200115151705), (20200116024319), (20200127033742), (20200128032134), (20200210202655), (20200212175538), (20200212183409), (20200213192845);
 
