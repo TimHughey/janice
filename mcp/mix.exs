136d5ee0ebc194ee9e29b4ccd679072a858715de
Tim Hughey <timhughey@mac.com>
2020-03-06 18:14:25 -0500

    MCP Version Bump For Defect Fix

--- mcp/mix.exs
+++ mcp/mix.exs
@@ -25,7 +25,7 @@ defmodule Mcp.Mixfile do
   def project do
     [
       app: :mcp,
-      version: "0.1.46",
+      version: "0.1.47",
       elixir: "~> 1.10",
       deps: deps(),
       releases: releases(),
