13984f192a151a4e67d923af610046ba755669e9
Tim Hughey <timhughey@mac.com>
2020-04-12 20:43:32 -0400

    Version Bump To Encapsulate Switch to live DNS Subdomain

--- mcp/mix.exs
+++ mcp/mix.exs
@@ -25,7 +25,7 @@ defmodule Mcp.Mixfile do
   def project do
     [
       app: :mcp,
-      version: "0.1.72",
+      version: "0.1.73",
       elixir: "~> 1.10",
       deps: deps(),
       releases: releases(),
